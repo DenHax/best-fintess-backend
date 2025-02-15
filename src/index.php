a09a0bff89c21bcbbba6f11367ed6849670b77c8
DenHax <whatxpw@gmail.com>
2025-02-15 12:41:50 +0300

    refactor: data gets from input in post-method, add get-method to get data from csv-file

--- src/index.php
+++ src/index.php
@@ -1,25 +1,55 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $name = trim($_POST['name'] ?? '');
-  $email = trim($_POST['email'] ?? '');
 
-  $csvFile = "data.csv";
+header('Content-Type: application/json');
 
-  $dataRow = [$name, $email];
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestMethod = $_SERVER['REQUEST_METHOD'];
 
-  if (($file = fopen($csvFile, 'a')) !== false) {
-    fputcsv($file, $dataRow);
+if ($requestMethod === 'GET') {
+    $csvFile = __DIR__ .  './static/train.csv';
+
+    if (!file_exists($csvFile)) {
+        http_response_code(404);
+        echo json_encode(['status' => 'error', 'message' => 'CSV file not found']);
+        exit;
+    }
+
+    $file = fopen($csvFile, 'r');
+
+    $data = [];
+
+    while (($row = fgetcsv($file, 1000, ',')) !== false) {
+        $data[] = [
+            'name' => $row[0],
+            'email' => $row[1]
+        ];
+    }
+
     fclose($file);
-    $message = 'Данные успешно сохранены';
-  }
-  else {
-    $message = 'Ошибка при сохранении';
-  }
-}
-?>
 
+    echo json_encode($data);
 
-  <form action="" method="POST">
-    <label for="name">Имя: </label>
-    <input type="text" name="name" id="name">
+} elseif ($requestMethod === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    if (json_last_error() === JSON_ERROR_NONE) {
+        $name = $data['name'] ?? null;
+        $email = $data['email'] ?? null;
+
+        $csvFile = __DIR__ . '/static/train.csv';
+
+        $dataRow = [$name, $email];
+
+        if (($file = fopen($csvFile, 'a')) !== false) {
+            fputcsv($file, $dataRow);
+            fclose($file);
+            $response = [
+                'status' => 'success',
+                'message' => 'POST request received',
+            ];
+            echo json_encode($response);
+        }
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['status' => 'error', 'message' => 'Method Not Allowed']);
 }
