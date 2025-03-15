6d0815a42d9ca6b382ea6aa4e170e9aaa944af8f
DenHax <whatxpw@gmail.com>
2025-03-16 01:25:14 +0300

    add: helper class as loader to upload and download content like avatar

---
+++ src/Helpers/Loader.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Core\ContentLoader;
+
+class Loader
+{
+    /** Get file that will be upload to /upload/:uploadType directory with random filename and return string to this file */
+    public function uploadImage($avatar, $uploadType): string | null
+    {
+        $uploadPath = '../storage/upload/';
+        if (!empty($avatar)) {
+            $fileTmpPath = $_FILES[$uploadType]['tmp_name'];
+            $fileName = $_FILES[$uploadType]['name'];
+            $fileSize = $_FILES[$uploadType]['size'];
+            $fileType = $_FILES[$uploadType]['type'];
+            $fileNameCmps = explode(".", $fileName);
+            $fileExtension = strtolower(end($fileNameCmps));
+
+            $allowedfileExtensions = ['jpg', 'gif', 'png', 'jpeg'];
+            $allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+            if (in_array($fileExtension, $allowedfileExtensions) && in_array($fileType, $allowedMimeTypes)) {
+                $newFileName = uniqid() . '.' . $fileExtension;
+                $path = $uploadPath . '/' . $uploadType . '/' . $newFileName;
+
+                if (move_uploaded_file($fileTmpPath, $path)) {
+                    return $path;
+                } else {
+                    return null;
+                }
+            }
+        } else {
+            $defaultPath = $uploadPath . '/' . $uploadType . '/' .'default.jpg';
+            return $defaultPath;
+        }
+    }
+
+    public function getImage($path)
+    {
+
+    }
+}
