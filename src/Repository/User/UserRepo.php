3d3fbb25da0a6d42d95f47d1655b903af025a0a3
DenHax <whatxpw@gmail.com>
2025-03-15 23:40:29 +0300

    refactor: user repo (part of model) create new user and make him a trainer if he is one

--- src/Repository/User/UserRepo.php
+++ src/Repository/User/UserRepo.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Repository\User;
+namespace App\Repository\User;
 
 use App\Core\Database;
-use Domain\User\User;
+use App\Domain\User\User;
 use PDO;
 
 class UserRepo
@@ -14,6 +14,7 @@ class UserRepo
     {
         $this->pdo = Database::connect($db_conn_info);
     }
+
     public function getPDO(): PDO
     {
         return $this->pdo;
@@ -22,34 +23,61 @@ class UserRepo
     public function addUser(User $user): string
     {
         $stmt = $this->pdo->prepare("
-            INSERT INTO users (
-                user_uuid,
-                user_surname,
-                user_firstname,
-                user_age,
-                user_height,
-                user_weight,
-                user_hash_password
+            INSERT INTO client (
+                client_surname,
+                client_firstname,
+                client_gender,
+                client_age,
+                client_height,
+                client_weight,
+                client_phone,
+                client_hash_password,
+                client_avatar_path
             ) VALUES (
-                :user_uuid,
-                :user_surname,
-                :user_firstname,
-                :user_age,
-                :user_height,
-                :user_weight,
-                :user_hash_password
+                :client_surname,
+                :client_firstname,
+                :client_gender,
+                :client_age,
+                :client_height,
+                :client_weight,
+                :client_phone,
+                :client_hash_password,
+                :client_avatar_path
             )
+            RETURNING client_uuid;
         ");
 
         $stmt->execute([
-            ':user_uuid' => $user->getUserUuid(),
-            ':user_surname' => $user->getUserSurname(),
-            ':user_firstname' => $user->getUserFirstname(),
-            ':user_age' => $user->getUserAge(),
-            ':user_height' => $user->getUserHeight(),
-            ':user_weight' => $user->getUserWeight(),
-            ':user_hash_password' => $user->getUserHashPassword(),
+                ':client_surname' => $user->getUserSurname(),
+                ':client_firstname' => $user->getUserFirstname(),
+                ':client_gender' => $user->getUserGender(),
+                ':client_age' => $user->getUserAge(),
+                ':client_height' => $user->getUserHeight(),
+                ':client_weight' => $user->getUserWeight(),
+                ':client_phone' => $user->getUserPhone(),
+                ':client_hash_password' => $user->getUserHashPassword(),
+                ':client_avatar_path' => $user->getUserAvatarPath(),
         ]);
+        $user->setUserUuid($stmt->fetchColumn());
+        error_log($user->getUserUuid());
+
+        if ($user->getUserIsTrainer() !== false) {
+            $trainer_role_id = 1;
+            $stmt = $this->pdo->prepare("
+              INSERT INTO client_role (
+                client_id,
+                role_id
+              ) VALUES (
+                :client_uuid,
+                :role_id
+              )
+            ");
+
+            $stmt->execute([
+              ':client_uuid' => $user->getUserUuid(),
+              ':role_id' => $trainer_role_id,
+            ]);
+        }
 
         return $user->getUserUuid();
     }
