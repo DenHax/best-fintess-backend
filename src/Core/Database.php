10a71e7fd0c3f41e456f48eda2a6f565d3ab5bce
DenHax <whatxpw@gmail.com>
2025-03-15 23:37:27 +0300

    refactor: remove useless files and move database config to core

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -1,22 +1,29 @@
 <?php
 
-$config = parse_ini_file('./config.ini', true);
+namespace App\Core;
 
 use PDO;
 use PDOException;
 
-$dbHost = $config['database']['DB_HOST'];
-$dbUsername = $config['database']['DB_USER'];
-$dbPassword = $config['database']['DB_PASS'];
-$dbName = $config['database']['DB_NAME'];
+class Database
+{
+    private static ?PDO $pdo = null;
+    public static function connect(array $db_conn_info): PDO
+    {
+        if (self::$pdo === null) {
 
-error_log($dbHost);
-
-try {
-    $pdo = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUsername, $dbPassword);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    error_log("Success connection to db");
-} catch (PDOException $e) {
-    $errorStr = "Connection failed: " . $e->getMessage();
-    error_log($errorStr);
+            $dsn = "pgsql:host={$db_conn_info['DB_HOST']};dbname={$db_conn_info['DB_NAME']}";
+            $db_pass = $db_conn_info["DB_PASS"];
+            $db_user = $db_conn_info["DB_USER"];
+            try {
+                self::$pdo = new PDO($dsn, $db_user, $db_pass);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                error_log("Success connection to db");
+            } catch (PDOException $e) {
+                $errorStr = "Connection failed: " . $e->getMessage();
+                error_log($errorStr);
+            }
+        }
+        return self::$pdo;
+    }
 }
